157ec19599b339924b679d60f5f3d7bf2873942f
vashinip <vashinij@gmail.com>
2024-04-19 06:17:04 +0530

    jpg added

---
+++ AI2/image_grey_conversion.py
@@ -0,0 +1,13 @@
+import cv2
+image = cv2.imread("ColourImage.jpg")
+grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('original', image)
+cv2.imshow('Gray', grayImage)
+
+cv2.imwrite('graynew.png', grayImage)
+
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
+print(image.shape)
+print(image.size)
